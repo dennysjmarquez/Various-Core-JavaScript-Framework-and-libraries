095e63bbae3314e20f79e4f447f61185f7fe720f
Dennys José Márquez Reyes <dennysjmarquez@gmail.com>
2019-11-18 12:59:47 -0400

    commit init

---
+++ inline-comment-bug.js
@@ -0,0 +1,34 @@
+function test5() {
+    if (sadfsadf/** dsfdsf */)/* dsfdsf */{
+        skjfdladkfs;
+        skjfdladkfs;/* dsfdsf */
+        this.traceToString = function (trace)/*@explore*/
+        {}
+    }
+    
+    try {
+		sdfsadf;
+	} catch (e) /*@explore*/
+	{ /*@explore*/
+		dump("FBL Fails " + e + "\n"); /*@explore*/
+	}/*@explore*/
+}
+/*@explore*/
+function test4() {/*esssxplore*/
+    return 10000/*test*/+100;
+}
+
+function test() {
+    return /*test*/10000;
+}
+
+function test2() {
+    return 100+/*test*/10000;
+}
+
+function test3() {
+    return /*test*/10000+100;
+}
+
+
+
---
+++ strings.js
@@ -0,0 +1,13 @@
+function test(input) {
+    var sql =
+        `SELECT *
+        FROM my_table
+        LIMIT 1;`;
+}
+
+function test(input) {
+    var sql =
+    "SELECT *
+                FROM my_table
+        LIMIT 1;";
+}
